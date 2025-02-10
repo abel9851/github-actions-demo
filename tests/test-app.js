f81cef760e56cc822104d2fd02c04045a08a40d8
HeejunShin <abel9851@gmail.com>
2025-02-10 21:42:39 +0900

    feat: add github action secrets

--- tests/test-app.js
+++ tests/test-app.js
@@ -7,12 +7,14 @@ chai.use(chaiHttp);
 
 const app = require('../index.js');
 
+const apiKey = process.env.API_KEY || 'default-for-local-testing';
+
 describe('GET /', () => {
   it('should respond with hello world', (done) => {
     chai.request(app)
       .get('/')
       .end((err, res) => {
-      // there should be no errors
+        // there should be no errors
         should.not.exist(err);
         // there should be a 200 status code
         res.status.should.equal(200);
@@ -20,5 +22,10 @@ describe('GET /', () => {
         res.type.should.equal('text/plain');
         done();
       });
+  });
+
+  it('should have valid API key', () => {
+    apiKey.should.be.a('string');
+    apiKey.length.should.be.at.least(10); // 최소 길이 체크
   });
 });
